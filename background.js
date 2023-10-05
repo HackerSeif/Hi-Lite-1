2304b50bf484a4605b3eb92503583a4c45808fcf
BIGAD GABER <BIGADGABER@icloud.com>
2023-10-05 13:12:49 -0500

    Setup tab query/ API request for extracting text.

--- background.js
+++ background.js
@@ -5,3 +5,10 @@ chrome.action.onClicked.addListener((tab) => {
             console.error("Error posting to server:", error);
     });
 });
+chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    ...
+    fetch('http://localhost:3000/', {
+        ...
+    })
+    ...
+});
