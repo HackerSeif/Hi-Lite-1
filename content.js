7da493a417dcd5253cb4a8fd56268c88af43ad5a
BIGAD GABER <BIGADGABER@icloud.com>
2023-10-05 13:08:06 -0500

    Implement text highlight logic in content script.

---
+++ content.js
@@ -0,0 +1,4 @@
+function escapeRegex(string) { ... }
+function highlightTextInNode(node, searchText) { ... }
+function highlightSentences(sentences) { ... }
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { ... });
