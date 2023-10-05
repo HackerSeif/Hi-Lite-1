bfd4e9ca8903dcacb3de5a3fa1abdbf308cc9e43
BIGAD GABER <BIGADGABER@icloud.com>
2023-10-05 13:12:05 -0500

    Implement main logic for: highlighting sentences and adding chrome message listener.

--- content.js
+++ content.js
@@ -2,3 +2,9 @@ function escapeRegex(string) { ... }
 function highlightTextInNode(node, searchText) { ... }
 function highlightSentences(sentences) { ... }
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { ... });
+function highlightSentences(sentences) {
+    ...
+}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    ...
+});
